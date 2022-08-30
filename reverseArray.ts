c91e91c23e6948417329388a9960681091a0e9af
Patajuade <manon.diana@icloud.com>
2022-08-30 02:46:47 +0200

    add reverse exercise

--- reverseArray.ts
+++ reverseArray.ts
@@ -1,12 +1,26 @@
-export const reverse=(tab:Array<number>)=>{
-//    var array = [1, 2, 3];
- 
-//_.reverse(array);
-// => [3, 2, 1]
- 
-//console.log(array);
-// => [3, 2, 1]
+export const reverse = (tab: Array<number>) => {
+    //    var array = [1, 2, 3];
 
-//prendre tab et renvoyer le tableau à l'envers
-return tab;
+    //_.reverse(array);
+    // => [3, 2, 1]
+
+    //console.log(array);
+    // => [3, 2, 1]
+
+    //prendre tab et renvoyer le tableau à l'envers
+
+    const result = Array(tab.length); //on donne une taille à result
+
+    //foreach modifie le tableau
+    tab.forEach((element, index) => { //pour tous les éléments de tab..
+        const resultIndex = tab.length - index - 1;
+        result[resultIndex] = element;
+    })
+    
+    //map fait une copie du tableau
+    /*const result = tab.map((element, index, arr) => { //pour tous les éléments de tab..
+        
+    })*/
+
+    return result;
 }
